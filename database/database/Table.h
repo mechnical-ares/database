8a6e51c988eae55ae6597ec90e973e1c5d304fe5
David <jiucko@users.noreply.github.com>
2015-04-19 16:58:17 +0800

    Update Table.h

--- database/database/Table.h
+++ database/database/Table.h
@@ -3,14 +3,18 @@
 //#include "Operation.h"
 using namespace std;
 
+struct columnsAndType{
+	string column; //记录的表头
+	string type; //数据
+};
 
 
 class Table{
 public:
-	Table(string, vector<string>,int, int);
+	Table(string, vector<columnsAndType>, int, int);
 private:
 	string tableName;
-	vector<string> columns;
+	vector<columnsAndType> columns;
 	int rowNum;
 	int colNum;
 	
