2cddb77b7b26fb8ccacd866cfb8eb19f184c2e91
reklanirs <tiansuixing@gmail.com>
2015-04-27 22:57:18 +0800

    B+ 1.9

--- database/database/BPlusTree.h
+++ database/database/BPlusTree.h
@@ -30,34 +30,10 @@ struct InsertConsequence
 #define LeafCap LeafNodeCapacity
 #define InCons InsertConsequence
 
-int char2int(char s[], int l, int r, int base = 10){
-	int ret = 0;
-	switch (base){
-	case 10:
-		if (l > r) return 0;
-		for (int i = l; i<=r; i++) if (s[i] != 0){
-			ret = ret*base + (s[i] - '0');
-		}
-		break;
-	case 128:
-	{
-		while (s[l] == 0 && l <= r) l++;
-		for (; l <= r; l++){
-			ret = ret * 128 + (int)s[l];
-		}
-	}
-
-	default: break;
-	}
-	return ret;
-}
-
-string char2string(char s[], int l, int r){
-	string ret = "";
-	for (int i = l; i<=r; i++) if (s[i] != 0)
-		ret.push_back(s[i]);
-	return ret;
-}
+int char2int(char s[], int l, int r, int base = 10);
+void int2char(char s[], int l, int r, int value, int base = 128);
+void string2chars(char s[], int l, int r, string &ss);
+string char2string(char s[], int l, int r);
 
 
 
@@ -70,20 +46,13 @@ public:
 	Node(NodeType _type = LEAF);
 	~Node();
 
-	void Write2Disk();
+	void Write2Disk(Disk &disk);
 };
 
 
 /*
 p.keys.size() == p.values.size() for all nodes p.
 ∀p∈LeafNode,∀k∈p.keys,∀k′∈p.next_page.keys,k<k′
-
-char s[512]:
-511: type
-510: size  //num of nodes, force to int
-509: num of attributes, (size of vector)
-0~499: keys and values, (20+10*8)*5
-500~508: next_page pointer
 
 
 char s[512]:
@@ -105,7 +74,7 @@ public:
 	vector<Value> values;
 	Page next_page;
 	Page prev_page;
-	void Write2Disk(int page);
+	void Write2Disk(int page, Disk &disk);
 };
 
 
@@ -116,13 +85,6 @@ p.keys.size() +1 == p.pointers.size()
 for all key k and the adjacent pointers, after(k) and after(k),
 k>max(keys(before(k)))
 k≤min(keys(after(k)))
-
-char s[512]:
-511: type
-510: size  //force to int
-0~509: keys + pointers , 10 char per
-0~9: first pointer
-500~509: the final pointer (if the node is full)
 
 char s[512]:
 511: type
@@ -140,7 +102,7 @@ public:
 	vector<Key> keys;
 	vector<Page> pointers;
 	
-	void Write2Disk(Page page);
+	void Write2Disk(Page page, Disk &disk);
 };
 
 
