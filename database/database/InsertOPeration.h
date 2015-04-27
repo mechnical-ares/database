cf80db45f6654706e03e061b4a80b1a0044067a2
TangZhihao <tjrtzh@gmail.com>
2015-04-27 16:49:28 +0800

    change the insert class

--- database/database/InsertOPeration.h
+++ database/database/InsertOPeration.h
@@ -2,10 +2,10 @@
 #include "stdafx.h"
 #include "Operation.h"
 
-class InsertOPeration :public Operation
+class InsertOperation :public Operation
 {
 public:
-	InsertOPeration();
+	InsertOperation();
 	virtual Table exec();
 	string tableName;
 	vector<Data> data;
