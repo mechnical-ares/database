01f0c5a19a49c10ea139f5e6a2a7edfbeac1bb56
kjwangseu <deidarawkj@gmail.com>
2015-04-27 17:14:25 +0800

    add lost files

---
+++ database/database/TableManagement.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+
+TableManagement::TableManagement()
+{
+}
+
+
+TableManagement::~TableManagement()
+{
+}
+int TableManagement::hasTable(string name){
+	for (int i = 0; i < TableInfomation.size(); i++)
+	{
+		if (TableInfomation.at(i).Tablename == name)
+			return true;
+	}
+	return false;
+}
+int TableManagement::hasColumn(string TableName, string ColumnName){
+	for (int i = 0; i < TableInfomation.size(); i++)
+	{
+		if (TableInfomation.at(i).Tablename == TableName)
+		{
+			for (int j = 0; j < TableInfomation.at(i).Columns.size(); j++)
+			{
+				if (TableInfomation.at(i).Columns.at(j).column_name == ColumnName)
+					return true;
+			}
+		}
+	}
+	return false;
+}
+int TableManagement::addTable(TableInfo t){
+	if (hasTable(t.Tablename))
+		return false;
+	else
+		TableInfomation.push_back(t);
+	return true;
+}
+vector<TableInfo> TableManagement::getTablebyColumn(string ColumnName){
+	vector<TableInfo> Tables;
+	for (int i = 0; i < TableInfomation.size(); i++)
+	{
+
+		for (int j = 0; j < TableInfomation.at(i).Columns.size(); j++)
+		{
+			if (TableInfomation.at(i).Columns.at(j).column_name == ColumnName)
+				Tables.push_back(TableInfomation.at(i));
+		}
+	}
+	return Tables;
+}
+vector<string> TableManagement::getColumnbyTable(string TableName){
+	vector<string> Column;
+	for (int i = 0; i < TableInfomation.size(); i++)
+	{
+		if (TableInfomation.at(i).Tablename == TableName)
+		{
+			for (int j = 0; j < TableInfomation.at(i).Columns.size(); j++)
+				Column.push_back(TableInfomation.at(i).Columns.at(j).column_name);
+			return Column;
+		}
+	}
+}
---
+++ database/database/TableManagement.h
@@ -0,0 +1,17 @@
+#pragma once
+struct TableInfo{
+	vector<ColumnTitle> Columns;
+	string Tablename;
+};
+class TableManagement
+{
+public:
+	TableManagement();
+	~TableManagement();
+	vector<TableInfo> TableInfomation;
+	int hasTable(string name);
+	int hasColumn(string TableName, string ColumnName);
+	vector<TableInfo> getTablebyColumn(string Columnname);
+	vector<string> getColumnbyTable(string TableName);
+	int addTable(TableInfo);
+};
